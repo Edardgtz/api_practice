2fe70f68bea9f9ae454ae914f049509e951a754c
Eduardo Gutierrez <eduardo.gutierrez643@gmail.com>
2020-10-30 09:52:02 -0500

    added name header before landlord name, added address with addres header

--- api_1.rb
+++ api_1.rb
@@ -6,7 +6,11 @@ slumlord = HTTP.get("https://data.cityofchicago.org/resource/n5zj-r44u.json").pa
 puts "Here is a list of problem landlords:"
 i = 0
 while i < slumlord.length-1
-    puts  slumlord[i]["respondent"]
+    puts "Name:"
+    puts slumlord[i]["respondent"]
+    puts "Address:"
+    puts slumlord[i]["address"]
   i += 1 
 end
 # p slumlord[0]["respondent"]
+# pp slumlord
